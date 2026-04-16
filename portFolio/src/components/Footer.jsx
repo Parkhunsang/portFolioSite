4ba1c79bf42037464b7523027e46e8802a477552
Parkhunsang <daxi99@naver.com>
2026-04-16 22:37:15 +0900

    테스트 커밋

--- portFolio/src/components/Footer.jsx
+++ portFolio/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="site-footer">
       <div className="layout-shell">
-        <p className="site-footer__copy">@ 2026 HUNSANG PORTFOLIO</p>
+        <p className="site-footer__copy">@ 2026 HUNSANG PORTFOLIO.</p>
       </div>
     </footer>
   );
